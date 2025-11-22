fe94d64aa778e87311a41f4c146212b2b079eba8
theabhipatel <theabhipatel.co@gmail.com>
2025-11-22 21:48:47 +0530

    chore: add some code changes to test live backend

--- src/server.ts
+++ src/server.ts
@@ -42,7 +42,7 @@ app.use("/api/todos", todoRoutes);
 app.get("/", (req, res) => {
   res
     .status(200)
-    .json({ success: true, message: "Welcome to the TheAbhiPatel era 🚀." });
+    .json({ success: true, message: "Welcome to the TheAbhiPatel era 🚀🚀." });
 });
 // Health check
 app.get("/health", (req, res) => {
